e62f26328389dbfd97cd7bae3ac0ab34869a1e0b
Stormy0611 <devstar0611@gmail.com>
2023-05-22 04:33:36 -0800

    second

--- professor.py
+++ professor.py
@@ -1,4 +1,4 @@
-Apply a uniform pressure load
+# Apply a uniform pressure load
 mapdl.nsel("S", "LOC", "Z", 1)
 mapdl.d("ALL", "PRES", 1)
 
@@ -18,10 +18,10 @@ hydrostatic_stress = np.zeros(num_nodes)
 hydrostatic_strain = np.zeros(num_nodes)
 
 for i in range(num_nodes):
-nnum = mapdl.post_processing.nnum[i]
-hydrostatic_stress[i] = mapdl.post_processing.nodal_stress(1, "S", nnum)
-hydrostatic_strain[i] = mapdl.post_processing.nodal_stress(1, "EPTO", nnum)
+    nnum = mapdl.post_processing.nnum[i]
+    hydrostatic_stress[i] = mapdl.post_processing.nodal_stress(1, "S", nnum)
+    hydrostatic_strain[i] = mapdl.post_processing.nodal_stress(1, "EPTO", nnum)
 
 # Print the hydrostatic stress and strain
 for i in range(num_nodes):
-print(f"Node {i+1}: Hydrostatic Stress = {hydrostatic_stress[i]}, Hydrostatic Strain = {hydrostatic_strain[i]}")
+    print(f"Node {i+1}: Hydrostatic Stress = {hydrostatic_stress[i]}, Hydrostatic Strain = {hydrostatic_strain[i]}")    
